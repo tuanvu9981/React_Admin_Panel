173708fee6563cd5bb68c589332342974c957d5d
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2022-04-22 00:29:22 +0700

    fix avatar navbar and add something in new-product-person page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import "./new.scss";
 
 function New(){
     return (
-        <div>
-            new
+        <div className="new">
+            <Sidebar/>
+            <div className="newContainer">
+                <Navbar/>
+                <div className="top">
+                    <h1>Add New User</h1> 
+                </div>
+                <div className="bottom">
+                    <div className="left">
+                        <img
+                            src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                            alt=""
+                            
+                        />
+                    </div>
+                    <div className="right">right</div>
+                </div>
+            </div>
         </div>
     )
 }
